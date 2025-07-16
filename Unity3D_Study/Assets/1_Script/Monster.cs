0a5d5ce27f0600ec5b567dc3593df9b07fcb5f44
Gangmin.Lee <wkdbwjr21@gmail.com>
2025-07-16 17:20:31 +0900

    [Add] New Scean Create
    카메라 연출 연습을 위한 새로운 씬 생성

--- Unity3D_Study/Assets/1_Script/Monster.cs
+++ Unity3D_Study/Assets/1_Script/Monster.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    [Header("컴포넌트")]
     [SerializeField] Animator anicon;
     [SerializeField] ParticleSystem hitEffect;
 
+    // 외부에서 데미지 입힐 때 호출
     public void Damaged()
     {
         anicon.SetTrigger("DAMAGED");
-        // 이 함수 안에선 애니메이션 트리거만 작동
     }
 
     // 애니메이션 이벤트에서 호출할 함수
@@ -18,8 +19,13 @@ public class Monster : MonoBehaviour
     {
         if (hitEffect != null)
         {
-            hitEffect.Play();
+            // 자식 파티클까지 모두 정지 및 클리어 후 재생
+            hitEffect.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            hitEffect.Play(true);
+        }
+        else
+        {
+            Debug.LogWarning("hitEffect가 할당되지 않았습니다!", this);
         }
     }
 }
-
